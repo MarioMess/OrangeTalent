34cb0f9c794721ca07dda629d3c713dc80e5d994
Mário Gonçalves da Silva <mariskovisk@gmail.com>
2021-06-18 22:48:34 -0300

    att

--- src/main/java/com/example/OrangeTalents/controller/UsuarioController.java
+++ src/main/java/com/example/OrangeTalents/controller/UsuarioController.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
